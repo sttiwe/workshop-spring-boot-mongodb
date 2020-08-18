b91e6dc3710826d56e9c5941f6d9357dc305ff00
sttiwe <sttiwew@gmail.com>
2020-08-18 13:29:17 -0300

    Insercao de usuario com POST

--- src/main/java/com/sttiwe/workshopmongo/services/UserService.java
+++ src/main/java/com/sttiwe/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sttiwe.workshopmongo.domain.User;
+import com.sttiwe.workshopmongo.dto.UserDTO;
 import com.sttiwe.workshopmongo.repository.UserRepository;
 import com.sttiwe.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTo) {
+		return new User(objDTo.getId(), objDTo.getName(), objDTo.getEmail());
 	}
 }
