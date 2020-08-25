3eca9871ce8499daa747563f64f4c01af8bfe927
sttiwe <sttiwew@gmail.com>
2020-08-25 11:23:05 -0300

    Obtendo um post por id

---
+++ src/main/java/com/sttiwe/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.sttiwe.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sttiwe.workshopmongo.domain.Post;
+import com.sttiwe.workshopmongo.repository.PostRepository;
+import com.sttiwe.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
