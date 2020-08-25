e3a3a0808b09fdd222e2548699072c7fafff4751
sttiwe <sttiwew@gmail.com>
2020-08-25 09:10:08 -0300

    Referenciando os posts do usuario

--- src/main/java/com/sttiwe/workshopmongo/config/Instantiation.java
+++ src/main/java/com/sttiwe/workshopmongo/config/Instantiation.java
@@ -43,6 +43,10 @@ public class Instantiation implements CommandLineRunner{
 		
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		ellen.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(ellen);
+		
 	}
 
 }
