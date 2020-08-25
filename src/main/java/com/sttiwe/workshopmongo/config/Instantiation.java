0168c82dbb335f2f07b16b67f522007f06e87100
sttiwe <sttiwew@gmail.com>
2020-08-25 11:49:40 -0300

    Acresentando comentarios aos posts

--- src/main/java/com/sttiwe/workshopmongo/config/Instantiation.java
+++ src/main/java/com/sttiwe/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.sttiwe.workshopmongo.domain.Post;
 import com.sttiwe.workshopmongo.domain.User;
 import com.sttiwe.workshopmongo.dto.AuthorDTO;
+import com.sttiwe.workshopmongo.dto.CommentDTO;
 import com.sttiwe.workshopmongo.repository.PostRepository;
 import com.sttiwe.workshopmongo.repository.UserRepository;
 
@@ -41,6 +42,12 @@ public class Instantiation implements CommandLineRunner{
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(ellen));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei faliz hoje!", new AuthorDTO(ellen));
 		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(pandora));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(sttiwe));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(pandora));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
