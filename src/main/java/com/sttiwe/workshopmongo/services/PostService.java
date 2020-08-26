4b41772f9dea2e6e38f45d5c122d6828fdd45ce3
sttiwe <sttiwew@gmail.com>
2020-08-26 09:17:05 -0300

    Consulta simples com @Query

--- src/main/java/com/sttiwe/workshopmongo/services/PostService.java
+++ src/main/java/com/sttiwe/workshopmongo/services/PostService.java
@@ -23,7 +23,8 @@ public class PostService {
 	}
 	
 	public List<Post> findByTitle(String text){
-		return repo.findByTitleContainingIgnoreCase(text);
+		return repo.searchTitle(text);
+		//return repo.findByTitleContainingIgnoreCase(text);
 	}
 	
 }
